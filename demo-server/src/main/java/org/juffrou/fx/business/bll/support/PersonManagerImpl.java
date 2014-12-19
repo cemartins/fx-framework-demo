a4b19dfcea552e0d1a003de0880c1efb38b12edc
Carlos Martins <carlos.mfa.martins@gmail.com>
2014-12-19 00:33:57 +0000

    configure service exporter layer

--- demo-server/src/main/java/org/juffrou/fx/business/bll/support/PersonManagerImpl.java
+++ demo-server/src/main/java/org/juffrou/fx/business/bll/support/PersonManagerImpl.java
@@ -2,7 +2,9 @@ package org.juffrou.fx.business.bll.support;
 
 import org.juffrou.fx.business.bll.PersonManager;
 import org.juffrou.fx.business.dom.Person;
+import org.springframework.stereotype.Service;
 
+@Service("personManager")
 public class PersonManagerImpl extends AbstractManagerImpl<Person> implements PersonManager {
 
 }
